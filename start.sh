37e92ee26dec427bd143a5f02e9d53beed168864
AglBot <146909834+AglBot@users.noreply.github.com>
2023-10-04 10:27:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/AglBot/AGL_BoT.git /AGL_BoT 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /AGL_BoT 
 fi
-cd /VJ-FILTER-BOT 
+cd /AGL_BoT 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
